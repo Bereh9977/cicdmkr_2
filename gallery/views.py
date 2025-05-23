34670dedfdef0e88be6708d3d25f041f860a3628
Назар Берещук <nazarberehklas@gmail.com>
2025-05-23 15:34:07 +0300

    Added second view

--- gallery/views.py
+++ gallery/views.py
@@ -4,3 +4,7 @@ from .models import Category, Image
 def gallery_view(request):
     categories = Category.objects.prefetch_related('image_set').all()
     return render(request, 'gallery.html', {'categories': categories})
+
+def image_detail(request, pk):
+    image = get_object_or_404(Image, id=pk)
+    return render(request, 'image_detail.html', {'image': image})
